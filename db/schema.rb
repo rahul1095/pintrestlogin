bfabbc6cf8d18e9e609a40c2f4f86bf08d23ba97
Rahul <jain.rahul1095@gmail.com>
2018-03-06 01:08:18 +0530

    addthese

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302155133) do
+ActiveRecord::Schema.define(version: 20180305003407) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 20180302155133) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -55,6 +61,7 @@ ActiveRecord::Schema.define(version: 20180302155133) do
     t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
